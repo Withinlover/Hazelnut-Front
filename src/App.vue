14bcec4496610a0df14b9df5e9de476708135f6f
Withinlover <2608491128@qq.com>
2021-05-17 23:16:30 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">
+        <img src="./assets/logo.png" />
+        <span>二手商品交易平台</span>
+      </router-link>
+      <div id="nav-links">
+        <router-link to="/" class="nav-links-item">Home</router-link>
+        <router-link to="/about" class="nav-links-item">About</router-link>
+      </div>
     </div>
-    <router-view/>
+    <h1>ElementUI + Axios 测试</h1>
+    <el-button type="primary" round>{{info}}</el-button>
+    <router-view />
   </div>
 </template>
 
@@ -18,15 +26,71 @@
 }
 
 #nav {
-  padding: 30px;
+  position: relative;
+  height: 2.5rem;
+  padding: 0.7rem 8rem;
+  border-bottom: solid #2c3e50;
+  text-align: left;
+  background-color: aliceblue;
 }
 
 #nav a {
   font-weight: bold;
+  text-decoration: none;
   color: #2c3e50;
+  display: inline-block;
+  font-size: 1.5rem;
+  line-height: 2.5rem;
+}
+
+#nav a img {
+  width: 2.5rem;
+  vertical-align: middle;
+  display: inline-block;
+}
+
+#nav a span{
+  color: #2c3e50;
+  display: inline-block;
 }
 
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+#nav-links {
+  position: absolute;
+  right: 8rem;
+  display: inline-block;
+}
+
+.nav-links-item {
+  padding-left: 1.2rem;
+}
+
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      info: "我后端呢？？？"
+    }
+  }, 
+  mounted () {
+    const url = 'http://123.57.194.168:8000/login/';
+    var formData = new FormData();
+    formData.append('username', 'aaa');
+    formData.append('password', '111');
+
+    this.axios.post(url, formData).then(res => {
+      this.info = '后端支棱起来了！！！'
+    })
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+}
+</script>
