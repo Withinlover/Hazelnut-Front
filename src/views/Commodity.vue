3ca1011b034dda7b36beb13e3af8cf7f12f38f4f
luxia <allxst@me.com>
2021-05-26 22:20:12 +0800

    ..

--- src/views/Commodity.vue
+++ src/views/Commodity.vue
@@ -4,20 +4,41 @@
       <el-input
         class="searchInput"
         v-model="input"
-        
         placeholder="请输入您想找的商品"
       ></el-input>
     </div>
+    <commodity-card></commodity-card >
+    <CommodityCard></CommodityCard>
+    <div class="pool">
+      <!-- all goods -->
+    </div>
+    <el-backtop
+      target=".page-component__scroll .el-scrollbar__wrap"
+    ></el-backtop>
   </div>
 </template>
 
 
 <style scoped>
 .Commodity {
-  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 .searchinput {
+  margin: 10px;
+  padding: 10px;
   width: 10px;
+  flex: 1;
+  /* cross-size: ; */
 }
 </style>
+
+<script>
+import CommodityCard from "../components/commodity/CommodityCard.vue";
+
+export default {
+  components: { CommodityCard },
+};
+</script>
