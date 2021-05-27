ad8e9798287002cc8bff97a4b8b562dc47dfb49a
luxia <allxst@me.com>
2021-05-27 13:54:17 +0800

    commodityCard

--- src/components/commodity/CommodityCard.vue
+++ src/components/commodity/CommodityCard.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="commodity-card">
-  <el-col :span="8" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 2 : 0">
-    <el-card :body-style="{ padding: '0px' }" shadow="hover">
-      <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
-      <div style="padding: 14px;">
-        <span class="title">{{title}}</span>
-        <div class="bottom clearfix">
-          
-          <el-button type="text" class="button">操作按钮</el-button>
+    <el-col :span="20">
+      <el-card :body-style="{ padding: '0px' }" shadow="hover">
+        <img :src="image_url" class="image" />
+        <div style="padding: 14px">
+          <span class="title">{{ title }}</span>
+          <div class="bottom clearfix">
+            <span class="rmb">¥</span>
+            <span class="price">{{ price }}</span>
+            <!-- <el-button type="text" class="button">操作按钮</el-button> -->
+          </div>
         </div>
-      </div>
-    </el-card>
-  </el-col>
-</el-row>
+      </el-card>
+    </el-col>
   </div>
 </template>
 
 <style>
 .bottom {
   margin-top: 13px;
+  margin-bottom: 13px;
   line-height: 12px;
+  float: right;
+  margin-left: 10px;
 }
 
 .button {
@@ -43,7 +46,20 @@
 }
 
 .title {
+  float: center;
   font-weight: bolder;
+  margin-bottom: 15px;
+}
+.price {
+  font-size: medium;
+  color: crimson;
+  font-family: "Droid Serif", "Droid Serif", serif;
+  font-weight: bolder;
+  padding-left: 3px;
+}
+.rmb {
+  color: crimson;
+  font-size: small;
 }
 </style>
 
@@ -52,7 +68,12 @@ export default {
   name: "CommodityCard",
   data() {
     return {
+      // title: "Burger汉堡这其实是一个超级长的标题blabla",
       title: "Burger汉堡",
+      image_url:
+        "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png",
+      price: 10.5,
+      detail_url: "",
     };
   },
   methods: {},
