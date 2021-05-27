b1efad84df375f71eefa63cfec4f7714be27c3a5
luxia <allxst@me.com>
2021-05-27 21:31:13 +0800

    not much. a hyperline to detail page

--- src/views/Commodity.vue
+++ src/views/Commodity.vue
@@ -8,11 +8,13 @@
         prefix-icon="el-icon-search"
       ></el-input>
     </div>
-    <commodity-card
-      imageUrl="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-      title="Burger汉堡包"
-      price="18.5"
-    />
+    <a href="http://localhost:8080/commodity/item">
+      <commodity-card
+        imageUrl="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+        title="Burger汉堡包"
+        price="18.5"
+      />
+    </a>
     <div class="pool">
       <!-- all goods -->
     </div>
@@ -90,5 +92,8 @@ import CommodityCard from "../components/commodity/CommodityCard.vue";
 
 export default {
   components: { CommodityCard },
+  data() {
+    return {};
+  },
 };
 </script>
