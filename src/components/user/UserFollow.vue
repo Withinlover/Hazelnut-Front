2f9dd45ea0f1c5ededbb5c637df1af6de09ced31
Matrix53 <1079207272@qq.com>
2021-06-08 18:28:24 +0800

    refactor: do some code decomposition

--- src/components/user/UserFollow.vue
+++ src/components/user/UserFollow.vue
@@ -2,31 +2,15 @@
   <div id="user-follow">
     <h1>我的关注</h1>
 
-    <div v-for="item in nowItems" :key="item.id" class="follow-item">
-      <el-avatar :size="50" :src="item.url"></el-avatar>
-      <div class="follow-info">
-        <div class="follow-info-name">
-          <p>{{item.name}}</p>
-          <el-rate
-            v-model="item.score"
-            disabled
-            show-score
-            text-color="#ff9900"
-            score-template="{value}"
-            v-if="item.score>=0">
-          </el-rate>
-          <p v-else>暂无交易记录</p>
-        </div>
-        <div class="follow-info-location">
-          <p>{{item.grade}}</p>
-          <p>{{item.location}}</p>
-        </div>
-      </div>
-      <el-button type="primary" @click="cancelFollow(item)">取消关注</el-button>
-      <el-divider></el-divider>
-    </div>
+    <follow-list
+      v-if="total"
+      :followList="follows"
+      :curPage="curPage"
+      :pageSize="pageSize"
+      @cancelFollow="cancelFollow">
+    </follow-list>
     <logo-hint
-      v-if="!total"
+      v-else
       :hint="hint">
     </logo-hint>
 
@@ -42,57 +26,12 @@
 h1{
   font-size: 30px;
 }
-.follow-item{
-  position: relative;
-  display: flex;
-  flex-direction: row;
-  flex-flow: wrap;
-  align-items: center;
-  width: 50rem;
-  margin: auto;
-}
-.el-divider{
-  margin: 0.8rem auto 0.8rem auto;
-}
-.follow-info{
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  position: relative;
-  left: 1rem;
-}
-.follow-info .follow-info-name{
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  margin: 0rem;
-}
-.follow-info .follow-info-name p{
-  margin: 0rem;
-  margin-right: 1rem;
-  font-weight:bolder;
-}
-.follow-info .follow-info-location{
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  margin: 0rem;
-}
-.follow-info .follow-info-location p{
-  margin-bottom: 0.1rem;
-  margin-right: 1rem;
-  margin-top: 0.1rem;
-}
-.el-button{
-  position: absolute;
-  right: 1rem;
-  top:0.5rem;
-}
 </style>
 
 <script>
 import PagBar from './nav/PagBar.vue'
 import LogoHint from './hint/LogoHint.vue'
+import FollowList from './follow/FollowList.vue'
 const map={
   grade:['暂未填写年级','大一','大二','大三','大四'],
   location:['暂未填写校区','沙河校区','学院路校区'],
@@ -102,68 +41,45 @@ const map={
 export default {
   components:{
     PagBar,
-    LogoHint
+    LogoHint,
+    FollowList
   },
   data(){
     return {
       pageSize:6,
-      currentPage:1,
-      allItems:[],
+      curPage:1,
+      follows:[],
       hint:'当前还没有关注任何人哦'
     }
   },
   computed:{
     total(){
-      return this.allItems.length
-    },
-    nowItems(){
-      let start=this.pageSize*(this.currentPage-1)
-      let end=Math.min(this.total,start+this.pageSize)
-      return this.allItems.slice(start,end)
+      return this.follows.length
     }
   },
   methods:{
     updatePage(page){
-      this.currentPage=page
+      this.curPage=page
     },
-    cancelFollow(item){
-      this.axios.post('/user/unfollow/',{
-        token:this.$store.state.token,
-        username:item.name
-      }).then(res =>{
-        this.$message({
-          message:'取消关注成功',
-          type:'success'
-        })
-        this.getFollowList()
-      },reason =>{
-        this.$message({
-          message:'请求超时，请检查网络设置',
-          type:'error'
-        })
-      })
-    },
-    getFollowList(){
-      this.axios.post('/user/followlist/',{
-        token:this.$store.state.token
-      }).then(res =>{
-        this.allItems=[]
-        let length=res.data.name.length
-        for(let i=0;i<length;i++){
-          let tmp={}
-          tmp.id=i
-          tmp.name=res.data.name[i]
-          tmp.grade=res.data.grade[i]<0? map.grade[0]:map.grade[res.data.grade[i]]
-          tmp.location=res.data.location[i]<0? map.location[0]:map.location[res.data.location[i]+1]
-          tmp.score=res.data.score[i]
-          tmp.url=res.data.url[i] === 'NULL'? map.defaultAvatar:res.data.url[i]
-          this.allItems.push(tmp)
-        }
-      })
+    cancelFollow(index){
+      this.follows.splice(index,1)
     }
   },
   mounted(){
-    this.getFollowList()
+    this.axios.post('/user/followlist/',{
+      token:this.$store.state.token
+    }).then(res =>{
+      let length=res.data.name.length
+      for(let i=0;i<length;i++){
+        let tmp={}
+        tmp.name=res.data.name[i]
+        tmp.grade=res.data.grade[i]<0? map.grade[0]:map.grade[res.data.grade[i]]
+        tmp.location=res.data.location[i]<0? map.location[0]:map.location[res.data.location[i]+1]
+        tmp.score=res.data.score[i]
+        tmp.avatarUrl=res.data.url[i] === 'NULL'? map.defaultAvatar:res.data.url[i]
+        this.follows.push(tmp)
+      }
+    })
   }
 }
 </script>
--- src/components/user/UserInfo.vue
+++ src/components/user/UserInfo.vue
@@ -2,85 +2,9 @@
   <div id="user-info">
     <h1>个人资料</h1>
 
-    <div id="show-form">
-      <el-row>
-        <el-col :span="4" >
-          <el-tag>
-            用户名
-            <i class="el-icon-user"></i>
-          </el-tag>
-        </el-col>
-        <el-col :span="8">
-          <p>{{showData.name}}</p>
-        </el-col>
-
-        <el-col :span="4">
-          <el-tag>
-            手机号码
-            <i class="el-icon-mobile-phone"></i>
-          </el-tag>
-        </el-col>
-        <el-col :span="8">
-          <p>{{showData.telephone}}</p>
-        </el-col>
-      </el-row>
-
-      <el-row>
-        <el-col :span="4">
-          <el-tag>
-            邮箱
-            <i class="el-icon-message"></i>
-          </el-tag>
-        </el-col>
-        <el-col :span="8">
-          <p>{{showData.email}}</p>
-        </el-col>
-
-        <el-col :span="4">
-          <el-tag>
-            性别
-            <i class="el-icon-male"></i>
-          </el-tag>
-        </el-col>
-        <el-col :span="8">
-          <p>{{showData.sex}}</p>
-        </el-col>
-      </el-row>
-
-      <el-row>
-        <el-col :span="4">
-          <el-tag>
-            微信号
-            <i class="el-icon-chat-dot-round"></i>
-          </el-tag>
-        </el-col>
-        <el-col :span="8">
-          <p>{{showData.wxid}}</p>
-        </el-col>
-
-        <el-col :span="4">
-          <el-tag>
-            年级
-            <i class="el-icon-office-building"></i>
-          </el-tag>
-        </el-col>
-        <el-col :span="8">
-          <p>{{showData.grade}}</p>
-        </el-col>
-      </el-row>
-
-      <el-row>
-        <el-col :span="4">
-          <el-tag>
-            校区
-            <i class="el-icon-location-outline"></i>
-          </el-tag>
-        </el-col>
-        <el-col :span="8">
-          <p>{{showData.location}}</p>
-        </el-col>
-      </el-row>
-    </div>
+    <user-show-form
+     ref="showForm">
+    </user-show-form>
 
     <div id="button">
       <el-row type="flex" justify="center">
@@ -98,7 +22,7 @@
        
     <user-info-form
       ref="infoForm"
-      @finishUpdate="getInfo">
+      @finishUpdate="handleInfoUpdate">
     </user-info-form>
 
     <user-avatar-form
@@ -111,37 +35,11 @@
 <style scoped>
 h1{
   font-size: 30px;
+  margin-bottom: 130px;
 }
-#show-form{
-  width: 760px;
-  height: 240px;
-  margin: auto;
-  margin-top: 130px;
-  padding: 20px 10px;
-  border-radius: 30px;
-  border: 1.5px solid #dddddd;
-  background: rgba(255, 255, 255,0.4);
-  backdrop-filter: blur(3px);
-}
-
-.el-tag{
-  height: 3rem;
-  width: 6.5rem;
-  font-size: 1rem;
-  padding-top: 0.5rem;
-}
-
 .el-row{
   margin-bottom: 1rem;
 }
-
-.el-col p{
-  color: rgb(181, 158, 140);
-  font-size: 1.5rem;
-  margin: auto;
-  padding-top: 0.5rem;
-}
-
 #button{
   margin: auto;
   margin-top: 10px;
@@ -153,7 +51,6 @@ h1{
   background: rgba(255, 255, 255,0.4);
   backdrop-filter: blur(3px);
 }
-
 #button .el-button{
   height: 45px;
   font-size: 1rem;
@@ -163,42 +60,13 @@ h1{
 <script>
 import UserInfoForm from './form/UserInfoForm.vue'
 import UserAvatarForm from './form/UserAvatarForm.vue'
-const map={
-  sex:['男','女'],
-  grade:['大一','大二','大三','大四'],
-  location:['沙河校区','学院路校区']
-}
+import UserShowForm from './form/UserShowForm.vue'
 
 export default {
     components:{
       UserAvatarForm,
-      UserInfoForm
-    },
-    data(){
-      return {
-        rawData:{
-          name:'',
-          email:'',
-          wxid: -1,
-          sex:-1,
-          grade:-1,
-          telephone: -1,
-          location:-1
-        }
-      }
-    },
-    computed:{
-      showData(){
-        return {
-          name:this.rawData.name,
-          email:this.rawData.email,
-          wxid:this.rawData.wxid == -1? '尚未填写微信号':this.rawData.wxid,
-          telephone:this.rawData.telephone == -1? '尚未填写手机号':this.rawData.telephone,
-          sex:this.rawData.sex == -1? '尚未填写性别':map.sex[this.rawData.sex],
-          grade:this.rawData.grade == -1? '尚未填写年级':map.grade[this.rawData.grade-1],
-          location:this.rawData.location == -1? '尚未填写校区':map.location[this.rawData.location]
-        }
-      }
+      UserInfoForm,
+      UserShowForm
     },
     methods:{
       clickChangeInfo(){
@@ -215,25 +83,12 @@ export default {
           type:'success'
         })
       },
-      getInfo(){
-        this.axios.post('/user/getinfo/',{
-          token:this.$store.state.token
-        })
-        .then(res => {
-          this.rawData=res.data
-        },reason =>{
-          this.$message({
-            message:'请求超时，请检查网络设置',
-            type:'error'
-          })
-        })
-      },
       handleAvatarUpdate(avatarUrl){
         this.$emit('updateAvatar',avatarUrl)
+      },
+      handleInfoUpdate(){
+        this.$refs.showForm.updateInfo()
       }
-    },
-    mounted(){
-      this.getInfo()
     }
 }
 </script>
