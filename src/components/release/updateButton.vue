8a2c030c11bce4aae133fd9292d448d62fbffe86
Withinlover <2608491128@qq.com>
2021-06-13 00:50:17 +0800

    Add Update Good/Demand Form

---
+++ src/components/release/updateButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-row>
+    <el-button type="text" @click="centerDialogVisible = true">
+      {{ text }}
+    </el-button>
+
+    <el-dialog
+      title="请重新填写信息"
+      :visible.sync="centerDialogVisible"
+      width="55%"
+      center>
+      <span>需要注意的是内容是默认不居中的</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="centerDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="centerDialogVisible = false">
+          确 定
+        </el-button>
+      </span>
+    </el-dialog>
+  </el-row>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+export default {
+  name: "updateButton",
+  props: ["initText"],
+  data() {
+    return {
+      text: this.initText,
+      centerDialogVisible: false
+    };
+  }
+};
+</script>
--- src/views/Release.vue
+++ src/views/Release.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="middle">
+    <update-button :initText="'一个奇怪的按钮'"/>
+    <el-divider></el-divider>
     <el-row type="flex" justify="space-around">
       <el-col :span="14">
         <release />
@@ -16,8 +18,9 @@
 
 <script>
 import release from "../components/release/releaseCard.vue";
+import UpdateButton from '../components/release/updateButton.vue';
 
 export default {
-  components: { release },
+  components: { release, UpdateButton },
 };
 </script>
