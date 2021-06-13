e7ccd097f7437a221c9228d893642fc13654e094
Withinlover <2608491128@qq.com>
2021-06-13 23:54:09 +0800

    I Don't Know

--- src/components/release/releaseCard.vue
+++ src/components/release/releaseCard.vue
@@ -60,6 +60,7 @@
           :headers="headers"
           :action="getUrl()"
           list-type="picture-card"
+          :before-upload="beforeAvatarUpload"
           :on-success="handleSuccess"
           :on-preview="handlePictureCardPreview"
           :on-remove="handleRemove"
@@ -249,6 +250,19 @@ export default {
         this.imgUrls[item] = { id: item, url: fileList[item].response.path };
       }
       console.log(this.imgUrls);
+    },
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === "image/jpeg";
+      const isPNG = file.type === "image/png";
+      const isLt5M = file.size / 1024 / 1024 < 5;
+
+      if (!isJPG && !isPNG) {
+        this.$message.error("上传图片只能是 JPG / PNG 格式!");
+      }
+      if (!isLt5M) {
+        this.$message.error("上传图片大小不能超过 5MB!");
+      }
+      return (isJPG || isPNG) && isLt5M;
     },
     getUrl() {
       if (this.form.type == 2)
