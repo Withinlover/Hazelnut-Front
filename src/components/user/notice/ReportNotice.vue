9d87ff1bdfa843feb028e23cb1feb0fc33f5fe9b
Matrix53 <1079207272@qq.com>
2021-06-14 11:27:38 +0800

    feat: design report notice

--- src/components/user/notice/ReportNotice.vue
+++ src/components/user/notice/ReportNotice.vue
@@ -2,19 +2,10 @@
   <base-notice
     :isRead="isRead"
     @read="read"
-    icon="el-icon-s-claim"
+    icon="el-icon-warning"
     message="商品举报通知">
-    <div
-      class="accept-box"
-      v-if="isAccept">
-      <div class="accept">
-        {{text}}
-      </div>
-    </div>
-    <div 
-      class="refuse-box"
-      v-else>
-      <div class="refuse">
+    <div class="report-box">
+      <div class="report">
         {{text}}
       </div>
     </div>
@@ -22,7 +13,7 @@
 </template>
 
 <style scoped>
-.accept-box{
+.report-box{
   margin: 0px auto 10px auto;
   width: 530px;
   display: flex;
@@ -32,36 +23,10 @@
   border: solid 1.5px #dddddd;
   border-radius: 20px;
 }
-.accept{
+.report{
   margin-top: 10px;
   margin-bottom: 10px;
   width: 75%;
-  font-size: 16px;
-  font-weight: bold;
-  color: #999999;
-}
-.after-rate{
-  margin-bottom: 5px;
-  padding: 5px 10px;
-  border: solid 1px #dddddd;
-  border-radius: 20px;
-  color: rgb(110, 91,80);
-  font-weight: bold;
-}
-.rate{
-  margin: 0px auto 10px 202px;
-}
-.refuse-box{
-  margin: 0px auto 10px auto;
-  width: 530px;
-  height: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: solid 1.5px #dddddd;
-  border-radius: 20px;
-}
-.refuse{
   font-size: 16px;
   font-weight: bold;
   color: #999999;
@@ -88,14 +53,6 @@ export default {
   data(){
     return {
       text:'',
-      isRate:true,
-      rate:0,
-      colors:['#99A9BF', '#F7BA2A', '#FF9900']
-    }
-  },
-  computed:{
-    isAccept(){
-      return this.text.indexOf('确认')>=0
     }
   },
   methods:{
@@ -104,26 +61,8 @@ export default {
         id:this.id
       }).then(res =>{
         this.text=res.data.text
-        this.isRate=res.data.score
       })
       this.$emit('read')
-    },
-    submitRate(){
-      this.axios.post('inform/scoring/',{
-        infoid:this.id,
-        score:this.rate
-      }).then(res =>{
-        this.isRate=true
-        this.$message({
-          type:'success',
-          message:'评分成功'
-        })
-      },reason =>{
-        this.$message({
-          type:'error',
-          message:'请求超时，请检查网络设置'
-        })
-      })
     }
   }
 }
